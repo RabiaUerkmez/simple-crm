410595f61e0eb92c975c32919785d45970e16be9
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-06-28 10:10:05 +0200

    add logo & firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers:
+    [provideRouter(routes), provideAnimationsAsync(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "simple-crm-f05eb", "appId": "1:434839343737:web:205df81f524471b86d7aa8", "storageBucket": "simple-crm-f05eb.appspot.com", "apiKey": "AIzaSyDcPlI7CVvMaKmFn7xvsqP5B-W7rkkeWcc", "authDomain": "simple-crm-f05eb.firebaseapp.com", "messagingSenderId": "434839343737" }))),
+    importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialogContent, MatDialogActions, MatDialogTitle } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatDialogContent, MatDialogActions, MatDialogTitle, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,12 +9,29 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms'
+import { Firestore, collection, doc, addDoc } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [FormsModule, MatDialogContent, MatDialogActions, MatButtonModule, MatDividerModule, MatDialogTitle, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatCalendar, MatIconModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatButtonModule,
+    MatDividerModule,
+    MatDialogTitle,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatCalendar,
+    MatIconModule,
+    MatProgressBarModule
+  ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
   providers: [provideNativeDateAdapter()],
@@ -23,10 +40,28 @@ export class DialogAddUserComponent {
 
   user = new User();
   birthDate!: Date;
+  loading = false;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>) { }
 
   saveUser() {
     this.user.birthDate = this.birthDate.getTime();
     console.log('Current user: ', this.user);
-    
+    this.loading = true;
+    addDoc(this.getUsersRef(), this.user.toJSON()).then((result) => {
+      console.log('Diese ', result);
+      this.loading = false;
+      this.dialogRef.close();
+    })
   }
+
+  getUsersRef() {
+    return collection(this.firestore, 'users');
+  }
+
+  // getSingleUserId(docId:string) {
+  //   return doc(collection(this.firestore, 'users'), docId)
+  // }
 }
